17bb837c626b1542ae7397735846957e1815264b
PorPlaTakKom <apiwat.snk@mail.kmutt.ac.th>
2022-10-20 02:20:56 +0700

    [CREATE] NEW BRANCH

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -4,7 +4,7 @@ import {Text, View} from "react-native";
 const SearchScreen = () => {
   return (
     <View>
-      <Text>SearchScreen</Text>
+      <Text>SearchScreen DEV</Text>
     </View>
   );
 };
