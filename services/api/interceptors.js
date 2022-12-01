8325d7a0799e3069f19545c771c9f1570858ff72
PorPlaTakKom <apiwat.sri@icloud.com>
2022-12-01 15:49:05 +0700

    UPDATE ACTION

--- services/api/interceptors.js
+++ services/api/interceptors.js
@@ -1,9 +1,22 @@
 import axiosInstance from "./config";
+import storages from "../storage/storages";
 
 const setup = (store) => {
 
   axiosInstance.interceptors.request.use(
     async (config) => {
+      try{
+        await storages.getData('userToken').then(res => {
+          console.log(JSON.parse(res).idToken)
+          if (res !== undefined) {
+            config.headers["Authorization"] = JSON.parse(res).idToken;
+            config.headers["content-type"] = 'application/json';
+          }
+        });
+      }catch (e) {
+        console.log('can not read idToken!!!')
+      }
+
       return config
     }, error => {
       return Promise.reject(error);
